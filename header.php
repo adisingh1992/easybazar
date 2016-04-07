9260d64b0a4de05697024c0019a0ac0e8d088707
adisingh1992 <adi.singh1992@gmail.com>
2016-04-08 01:24:25 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ЄѦՏүѣѦՀѦГ</title>
+        <meta charset="UTF-8"/>
+        <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+        <link href="layout.css" rel="stylesheet" type="text/css"/>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </head>
+    <body>
+        <header class="row">
+            <div class="c-1">
+                <div id="logo">
+                    <span>ЄѦՏү</span><span style="color:#fff;">ѣѦՀѦГ</span>
+                </div>
+            </div>
+            <div id="search" class="c-8">
+                <form action="search.php" method="get">
+                    <input type="text" name="search-txt" placeholder="Search for a product"/>
+                    <input type="submit" hidden/>
+                </form>
+            </div>
+            <a href="login.php" class="c-3" id="login-box">Hey there, Sign-In..!!</a>
+        </header>
